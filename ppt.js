eff815d1e483008d3c156c5799cc22ffb3f8f341
MarcelaMesaLeon <mardie05@hotmail.com>
2022-05-14 15:03:15 -0500

    Ajuste Condicionales

--- ppt.js
+++ ppt.js
@@ -6,7 +6,7 @@ const max = 3;
 let eleccionMaquina = Math.floor(Math.random()*(max-min+1)+min);
 alert(eleccionMaquina);
 //Condiciones para el juego deacuerdo a lo que asigne la maquina//
-if(eleccionMaquina=="1"){
+/*if(eleccionMaquina=="1"){
     if(eleccionUsuario=="1"){
         alert("Empatados!");
     }
@@ -38,6 +38,21 @@ if(eleccionMaquina=="3"){
     if(eleccionUsuario=="3"){
         alert("Empatados!");
     }
+}*/
+
+if(eleccionMaquina=="1"){
+    if(eleccionUsuario=="1"){
+        alert("Empatados!");
+    }
+    if(eleccionUsuario=="2"){
+        alert("Ganaste!");
+    }
+    if(eleccionUsuario=="3"){
+        alert("Perdiste!");
+    }
+}
+if(eleccionMaquina == "1" && eleccionUsuario == "1"){
+    alert("Empatados!");
 }
 //No olvidar el tipo de dato que se esta asignando y la comparacion que se realiza//
 const piedra = document.getElementById("piedra");
